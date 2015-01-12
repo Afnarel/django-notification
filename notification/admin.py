e9abff04b52482456c619f327fdc51194353197b
François Chapuis <afnarel@gmail.com>
2015-01-12 13:30:22 +0100

    Updated NoticeSetting admin

--- notification/admin.py
+++ notification/admin.py
@@ -8,7 +8,7 @@ class NoticeTypeAdmin(admin.ModelAdmin):
 
 
 class NoticeSettingAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "notice_type", "medium", "send"]
+    list_display = ["user", "notice_type", "medium", "send"]
 
 
 admin.site.register(NoticeQueueBatch)
