92abf824665072cc83b630ff4d3c074f5e89d319
François Chapuis <afnarel@gmail.com>
2015-01-12 13:11:56 +0100

    Moved the can_send function to a utils file

---
+++ notification/utils.py
@@ -0,0 +1,18 @@
+from models import NOTICE_MEDIA, NoticeType, NoticeSetting
+
+
+def can_send(user, notice_type_id, backend_name):
+    """
+    For instance: can_send(afnarel, 'ignilife_news', 'email')
+    """
+    notice_type = NoticeType.objects.get(label=notice_type_id)
+
+    backend_id = None
+    for bid, bname in NOTICE_MEDIA:
+        if bname == backend_name:
+            backend_id = bid
+            break
+    if backend_id is None:
+        return False
+
+    return NoticeSetting.for_user(user, notice_type, backend_id).send
